651f37e44b5d939003039d39f9e9a9f2bcfe7407
n3ndor <107216827+n3ndor@users.noreply.github.com>
2023-11-22 10:36:36 -0300

    phone mode for page 2

--- app/components/PageTwo.js
+++ app/components/PageTwo.js
@@ -20,18 +20,18 @@ const PageTwo = () => {
       width: '100vw', 
       height: '100vh', 
     }}>
-      <div className="flex justify-between w-[90%] gap-20 bg-gradient-to-b from-[#6a99c548] to-[#01016860] rounded-s-full h-[75%] overflow-visible translate-x-[2000px]">
-        <video autoPlay loop muted height={"auto"} width={"40%"}>
+      <div className="flex phone:flex-col justify-between w-[90%] gap-6 bg-gradient-to-b from-[#6a99c548] to-[#01016860] rounded-s-full h-[75%] overflow-visible translate-x-[2000px]">
+        <video autoPlay loop muted height={"auto"} width={"40%"} className="phone:w-full">
           <source src="/books.mp4" type="video/mp4" />
         </video>
-        <div className="flex flex-col justify-center items-end w-[50%] mr-10">
+        <div className="flex flex-col justify-center items-end w-[50%] mr-10 phone:items-start phone:w-full">
           <p className="text-highlight text-4xl font-semibold overflow-y-hidden">What&apos;s Your HERO Story?</p>
           <p className='flex flex-col'>
             <span className='my-3'>&apos;There are so many amazing stories that go untold. so many unique perspectives that break the mold.</span>
             <span className='my-3'>Why not build an ecosystem where wordsmiths&apos; tales unfold, and share with the world your literary gold.&apos;</span>
             <span className='my-3'>Fans link up with your favorite authors to add a copy of their limited edition content to your digital collection.</span>
           </p>
-          <button className="button-orange mt-4">Get started for free</button>
+          <button className="button-orange mt-10">Get started for free</button>
         </div>
         
       </div>
