f5317ab76ada84dfceb8d25ef93fc0e280ed7682
Nicholas Chikuji <chiku524@icloud.com>
2023-11-21 21:47:33 -0500

    fixed other 2 pages

--- app/components/PageTwo.js
+++ app/components/PageTwo.js
@@ -2,9 +2,6 @@
 
 import React, {useLayoutEffect} from 'react';
 
-import Image from "next/image";
-import '../components/css/page-two.css';
-
 import {gsap} from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
@@ -12,7 +9,7 @@ const PageTwo = () => {
   gsap.registerPlugin(ScrollTrigger);
 
   useLayoutEffect(() => {
-    gsap.to(".page-2-container", {scrollTrigger: {trigger: ".page-2-container", start: "5% bottom"}, x: 0, duration: 1.5});
+    gsap.to("div.rounded-s-full", {scrollTrigger: {trigger: "div.rounded-s-full", start: "5% bottom"}, x: 0, duration: 1.5});
   }, [])
 
   return (
@@ -23,18 +20,16 @@ const PageTwo = () => {
       width: '100vw', 
       height: '100vh', 
     }}>
-      <div className="flex justify-between w-[90%] gap-20 page-2-container">
-        <div className='video-container'>
-          <video autoPlay loop muted >
-            <source src="/books.mp4" type="video/mp4" />
-          </video>
-        </div>
-        <div className="flex flex-col justify-center items-end w-[50%] body-container">
-          <p className="text-highlight text-4xl font-semibold p-container">What&apos;s Your HERO Story?</p>
-          <p className='body-paragraph'>
-            <span>&apos;There are so amny amazing stories that go untold. so many unique perspectives that break the mold.</span>
-            <span>Why not build an ecosystem where wordsmiths&apos; tales unfold, and share with the world your literary gold.&apos;</span>
-            <span>Fans link up with your favorite authors to add a copy of their limited edition content to your digital collection.</span>
+      <div className="flex justify-between w-[90%] gap-20 bg-gradient-to-b from-[#6a99c548] to-[#01016860] rounded-s-full h-[75%] overflow-visible translate-x-[2000px]">
+        <video autoPlay loop muted height={"auto"} width={"40%"}>
+          <source src="/books.mp4" type="video/mp4" />
+        </video>
+        <div className="flex flex-col justify-center items-end w-[50%] mr-10">
+          <p className="text-highlight text-4xl font-semibold overflow-y-hidden">What&apos;s Your HERO Story?</p>
+          <p className='flex flex-col'>
+            <span className='my-3'>&apos;There are so many amazing stories that go untold. so many unique perspectives that break the mold.</span>
+            <span className='my-3'>Why not build an ecosystem where wordsmiths&apos; tales unfold, and share with the world your literary gold.&apos;</span>
+            <span className='my-3'>Fans link up with your favorite authors to add a copy of their limited edition content to your digital collection.</span>
           </p>
           <button className="button-orange mt-4">Get started for free</button>
         </div>
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,8 @@ import PageOne from "@/app/components/PageOne";
 import PageTwo from "@/app/components/PageTwo";
 import PageThree from "@/app/components/PageThree";
 import PageFour from "@/app/components/PageFour";
-import PageFive from "@/app/components/PageFive"
+import PageFive from "@/app/components/PageFive";
+import PageSix from "@/app/components/PageSix";
 import PageAnimationWrapper from "@/app/components/PageAnimationWrapper";
 import Nav from "./components/global/Nav";
 
@@ -16,6 +17,7 @@ export default function Home() {
         <PageThree />
         <PageFour />
         <PageFive />
+        <PageSix />
       </PageAnimationWrapper>
     </>
     
